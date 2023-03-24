944c5df9083e458856b27585a321ca7cf75b6109
Emmanuel Marcos de  Oliveira <116108389+emmanuelmarcosdeoliveira@users.noreply.github.com>
2023-03-24 15:48:32 -0300

    aula-06

---
+++ aula-06/aula06.js
@@ -0,0 +1,16 @@
+// Operadores condicionais
+let x = 5;
+let y = "5";
+
+
+//const resultado = 5 > 4 
+//const resultado = x <= y
+//const resultado = x == y // variáveis boleanas
+//const resultado  = x != y
+//const resultado = x === y
+// verifica se é do mesto tipo , number, string 
+let resultado = !true
+// acima temos uma negação a ! faz com o valor se inverta. 
+
+
+console.log(resultado)
