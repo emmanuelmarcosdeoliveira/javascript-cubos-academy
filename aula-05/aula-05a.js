13199d7b60ac95318a634b107e7592b16c5e5026
Emmanuel Marcos de  Oliveira <116108389+emmanuelmarcosdeoliveira@users.noreply.github.com>
2023-03-24 15:29:18 -0300

    aula-05

---
+++ aula-05/aula-05a.js
@@ -0,0 +1,17 @@
+//let resultado  = 5+4
+//let resultado = 5-4
+//let resultado = 5*4
+//let resultado = 5 / 4
+//let resultado  = 5 ** 4
+//let resultado  = 5 + 4 * 3 -1
+//let  resultado  = (5 + 4) * (3 -1)
+/* Ele ira fazer primeiro
+( )
+potenica
+multiplicação e divisão 
++ ou - 
+*/
+const x = 5
+const y = 6 
+let resultado  = 3 + x - y; // 02
+console.log(resultado);
