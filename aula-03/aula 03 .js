ab6d1ffbd05e8f4c2288589c1d9c210927597abd
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-03-24 08:26:12 -0300

    aula 04

---
+++ aula-03/aula 03 .js
@@ -0,0 +1,4 @@
+// Bem vndo ao Repli,
+// Comando usado para imprimir coisas na tela.
+console.log("Ola Mundo");
+console.log("Olá José")
---
+++ aula-04/aula4a.js
@@ -0,0 +1,12 @@
+// Variáveis 
+// Aula 04 
+const nome = "Emmanuel";
+// criando uma constante do tipo String.
+console.log(nome);
+//criando uma variável com let 
+let idade = 18;
+// mudando o valor da variavel idade
+idade = 43;
+console.log(idade)
+idade = "19"
+// atribuimos o valor de idade como string 
