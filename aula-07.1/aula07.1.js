46f1cb698c319b31143291c59faf52a626af1551
Emmanuel Marcos de  Oliveira <116108389+emmanuelmarcosdeoliveira@users.noreply.github.com>
2023-03-24 16:04:29 -0300

    aula-07.1

---
+++ aula-07.1/aula07.1.js
@@ -0,0 +1,19 @@
+// condicionais 
+// if (significa ( se )
+// else (significa (se não)
+const idade = 15;
+
+if (idade >= 18){
+// só executado se idade for maior ou igual a 18
+  console.log("Pode seguir pois é maior de idade");
+  
+}else {
+  // só é executado caso o o if seja falso (false)
+  console.log("Não é possivel sua entrada pois você é menor de idade!!")
+  
+}
+
+
+
+
+
